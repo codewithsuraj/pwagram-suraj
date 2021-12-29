8a7cf3c84b390b0a417f76020920afca82b00c25
Suraj Mahendrakar <suraj.mahendrakar@primussoft.com>
2021-12-29 13:58:00 +0530

    DevOps commit id

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -191,7 +191,7 @@ function createCard(data) {
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url(' + data.image + ')';
   cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '500px';
+  cardTitle.style.height = '512px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.style.color = 'white';
